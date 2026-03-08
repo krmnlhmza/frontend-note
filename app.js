d80aeec53a0a2e645812ec753757659e04fddbe5
Muhammed Hamza Karamanlı <muhammedhamzakaramanli@Muhammed-MacBook-Air.local>
2026-03-08 13:51:52 +0300

    Mac version_1

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!"); //consola yazar
+document.writeln("Goodbye, World! <br>"); //belgeye yazar
+document.write("Hello again, World!"); //belgeye yazar
+alert("sayfa ulaşılamıyor"); //uyarı penceresi açar
